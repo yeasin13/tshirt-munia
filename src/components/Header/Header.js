c17468338d120d725258a34e5f080d294b477a72
yeasin13 <yeasinarafat8001@gmail.com>
2022-04-03 21:59:18 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Welcome to tshirt mania!!</h2>
+        </div>
+    );
+};
+
+export default Header;<h2>Welcome to tshirt mania!!</h2>
